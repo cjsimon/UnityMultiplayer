6acc2ade0b99c7157e745737b4ae89b14b3d3056
Christopher Simon <cjsimon333@gmail.com>
2016-10-09 23:58:34 -0700

    Fixed Google VR and updated Demo Scene

---
+++ GameClient/GameClient/Assets/Scripts/Rotate.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class Rotate:MonoBehaviour {
+	void Start() {
+	}
+
+	void Update() {
+		transform.Rotate(15 * Vector3.right * Time.deltaTime, Space.World);
+		transform.Rotate(10 * Vector3.up * Time.deltaTime, Space.World);
+	}
+}
